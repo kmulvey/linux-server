8551c3d5cea43d068704651b4dc4b87b1d16a194
Kevin Mulvey <king.feruke@gmail.com>
2014-07-21 07:44:23 -0400

    Update hopdevil.pp

--- puppet/manifests/hopdevil.pp
+++ puppet/manifests/hopdevil.pp
@@ -3,6 +3,34 @@ class { '::ntp':
 	servers => [ 'time.nist.gov', 'time-d.nist.gov', 'nist.time.nosc.us', 'time-c.timefreq.bldrdoc.gov' ],
 	restrict => ['127.0.0.1'],
 }
+
+class { 'ssh':
+	storeconfigs_enabled => false,
+	server_options => {
+		'AllowTcpForwarding' => 'no',
+		'AllowUsers' => 'kmulvey',
+		#'Banner' => '/etc/issue',
+		'HostbasedAuthentication' => 'no',
+		'IgnoreRhosts' => 'yes',
+		'LogLevel' => 'INFO',
+		'PasswordAuthentication' => 'yes',
+		'PermitEmptyPasswords' => 'no',
+		'PermitRootLogin' => 'no',
+		'Port' => [22],
+		'RhostsAuthentication' => 'no',
+		'RhostsRSAAuthentication' => 'no',
+		'RSAAuthentication' => 'yes',
+		'ServerKeyBits' => '4096',
+		'StrictModes' => 'yes',
+		'UsePrivilegeSeparation' => 'yes',
+		'VerifyReverseMapping' => 'yes',
+		'X11Forwarding' => 'no',
+	},
+	client_options => {
+	  'ForwardAgent' => 'yes'
+	},
+}
+
 class desktop {
 	# below packages do not require configs
 	package { alpine: ensure => latest }
