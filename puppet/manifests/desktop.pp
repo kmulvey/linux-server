0f6742a2fcea3c9e7eeceebc75e06c402c5d2ef8
Kevin Mulvey <kmulvey@shutterstock.com>
2014-07-22 10:01:39 -0400

    break files into linux and desktop

---
+++ puppet/manifests/desktop.pp
@@ -0,0 +1,10 @@
+class desktop {
+	# below packages do not require configs
+	package { alpine: ensure => latest }
+	package { google-chrome-beta: ensure => latest }
+	package { google-droid-serif-fonts: ensure => latest }
+	package { google-droid-sans-fonts: ensure => latest }
+	package { google-droid-sans-mono-fonts: ensure => latest }
+	package { firefox: ensure => latest }
+	package { pidgin: ensure => latest }
+}
