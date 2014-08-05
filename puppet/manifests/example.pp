a7569c0161e3d4637393f4e27c4884a3351354b7
Kevin Mulvey <king.feruke@gmail.com>
2014-08-05 07:57:56 -0400

    get user going

--- puppet/manifests/example.pp
+++ puppet/manifests/example.pp
@@ -1,1 +1,14 @@
 import 'desktop.pp'
+
+vcsrepo { "/home/kmulvey":
+  ensure   => present,
+  provider => git,
+  source   => "https://github.com/kmulvey/dotfiles.git",
+}
+
+user { "kmulvey":
+	ensure     	=> present,
+	home		=> '/home/kmulvey',
+	shell      	=> "/bin/bash",
+	password 	=> '$6$S7itf5yd$oG/JJvRqBfc.DEk.PySNRJUvkzU00IvCByDzTssYoZpL.t2SKbkzOqfUYvaMpKwI79pztfTDDaEYVO1zZwf6j0'
+}
