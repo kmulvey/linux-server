c4ef36d98cbdcb47cf0ed9bade7770d4ceae11ec
Kevin Mulvey <kmulvey@linux.com>
2015-01-22 14:35:57 -0500

    only needed for desktop

--- deploy/deploy-puppet.sh
+++ deploy/deploy-puppet.sh
@@ -18,14 +18,14 @@ cat >> /etc/puppet/puppet.conf <<-EOF
 server = localhost
 EOF
 
-cat >> /etc/yum.repos.d/google-chrome.repo
-[google-chrome]
-name=google-chrome - \$basearch
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
-EOF
+#cat >> /etc/yum.repos.d/google-chrome.repo
+#[google-chrome]
+#name=google-chrome - \$basearch
+#baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+#EOF
 
 systemctl start puppetmaster
 
