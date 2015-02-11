6409d35fbd8c1573af4276a630b4101ffff8820a
Kevin Mulvey <kmulvey@linux.com>
2015-02-11 12:00:11 -0500

    rebuild script

---
+++ upgrade_kernel.sh
@@ -0,0 +1,10 @@
+git checkout master
+git pull
+cat `/bin/ls -t /boot/config-* | head -n1` > .config
+make oldconfig
+make -j`grep processor /proc/cpuinfo | wc -l`
+sudo cp arch/`uname -p`/boot/bzImage "/boot/vmlinuz-"`make kernelrelease`
+sudo make modules_install
+sudo cp System.map "/boot/System.map-"`make kernelrelease`
+sudo dracut "" `make kernelrelease`
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
