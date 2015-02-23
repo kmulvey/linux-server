a5a5fa6c0ebc056ee1204b67def9a2a824db4d03
Kevin Mulvey <kmulvey@linux.com>
2015-02-23 11:11:22 -0500

    usually defaults

--- upgrade_kernel.sh
+++ upgrade_kernel.sh
@@ -1,7 +1,7 @@
 git checkout master
 git pull
 cat `/bin/ls -t /boot/config-* | head -n1` > .config
-make oldconfig
+yes "" | make oldconfig
 make -j`grep processor /proc/cpuinfo | wc -l`
 sudo cp arch/`uname -p`/boot/bzImage "/boot/vmlinuz-"`make kernelrelease`
 sudo make modules_install
