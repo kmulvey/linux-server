c753deec6b968fd8a1ea2053b1bdad61f0c7b4d1
Kevin Mulvey <kmulvey@shutterstock.com>
2015-08-25 09:43:42 -0400

    rm hashed passwd

--- puppet/manifests/example.pp
+++ puppet/manifests/example.pp
@@ -10,5 +10,4 @@ user { "kmulvey":
 	ensure     	=> present,
 	home		=> '/home/kmulvey',
 	shell      	=> "/bin/bash",
-	password 	=> '$6$S7itf5yd$oG/JJvRqBfc.DEk.PySNRJUvkzU00IvCByDzTssYoZpL.t2SKbkzOqfUYvaMpKwI79pztfTDDaEYVO1zZwf6j0'
 }
